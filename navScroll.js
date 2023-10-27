36abcffa4ff86f65624d4b1200d5bd8fff566e43
Daniele Cagnoni <daniele.010@hotmail.it>
2023-10-27 17:02:24 +0200

    added js function interaction

--- navScroll.js
+++ navScroll.js
@@ -5,7 +5,11 @@ window.addEventListener("scroll", function () {
   if (scrollHeight > 330) {
     nav.classList.add("scrollDownColor");
     navButton.classList.add("scrollDownButtonColor");
+    nav.classList.remove("scrollUpColor");
+    navButton.classList.remove("scrollUpButtonColor");
   } else {
+    nav.classList.add("scrollUpColor");
+    navButton.classList.add("scrollUpButtonColor");
     nav.classList.remove("scrollDownColor");
     navButton.classList.remove("scrollDownButtonColor");
   }
